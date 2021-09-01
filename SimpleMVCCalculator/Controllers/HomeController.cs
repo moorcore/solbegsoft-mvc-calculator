87ee40ab82b6d615c402538b654c0963267bcce6
Maxim <maks96rush@yandex.ru>
2021-09-01 19:24:00 +0300

    init: starting the project

---
+++ SimpleMVCCalculator/Controllers/HomeController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SimpleMVCCalculator.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleMVCCalculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Calculator calc)
+        {
+            double a, b;
+
+            a = calc.value1;
+            b = calc.value2;
+
+            if (calc.calculate == "+")
+            {
+                calc.result = a + b;
+                calc.notification = "Addition:";
+                ViewData["Notification"] = calc.notification;
+            }
+            if (calc.calculate == "-")
+            {
+                calc.result = a - b;
+                calc.notification = "Substraction:";
+                ViewData["Notification"] = calc.notification;
+            }
+            if (calc.calculate == "x")
+            {
+                calc.result = a * b;
+                calc.notification = "Multiplication:";
+                ViewData["Notification"] = calc.notification;
+            }
+            if (calc.calculate == "÷")
+            {
+                if (b != 0)
+                {
+                    calc.result = a / b;
+                    calc.notification = "Division:";
+                    ViewData["Notification"] = calc.notification;
+                }  
+                else
+                {
+                    calc.notification = "Can not divide by zero!";
+                    ViewData["Notification"] = calc.notification;
+                }
+                    
+            }
+
+            ViewData["Result"] = calc.result;
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Info()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ SimpleMVCCalculator/Models/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleMVCCalculator.Models
+{
+    public class Calculator
+    {
+        public double value1 { get; set; }
+        public double value2 { get; set; }
+        public double result { get; set; }
+        public string calculate { get; set; }
+        public string notification { get; set; }
+    }
+}
